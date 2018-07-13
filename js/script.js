f30e57b8dabf6e30fb14bee48db5a1fbee816edb
kot-pirat <kot-pirat@bk.ru>
2018-07-13 13:23:38 +0300

    Готова вёрстка, кромме встриваемых видео и отзывов

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var customSelect, i, j, oldSelect, selectedItem, optionList, optionItem;
 //customSelect - блок, заменяющий select для стилизации; i, j - итераторы; oldSelect - select, который заменяют; optionList - список, заменяющий выпадющий список у select,
-//				optionItem - элемент выпадающего списка; selectedItem - выбранный элемент (блок, заменяющий select box)
+//				optionItem - элемент нового выпадающего списка; selectedItem - выбранный элемент (блок, заменяющий select box)
 customSelect =  document.getElementsByClassName('custom-select');
 
 for (i = 0; i < customSelect.length; i++){
@@ -14,16 +14,16 @@ for (i = 0; i < customSelect.length; i++){
 	optionList = document.createElement('ul');
 	optionList.setAttribute('class', 'select-items select-hide');
 	
-	for (j = 1; j < oldSelect.length; j++) {
+	for (j = 0; j < oldSelect.length; j++) {
 		optionItem = document.createElement('li');
 		optionItem.innerHTML = oldSelect.options[j].innerHTML;
 		optionItem.addEventListener('click', function(evt) {								//при клике на optionItem 
 			var optionItemLocal, i, k, oldSelectLocal, selectedItemLocal;
 			oldSelectLocal = this.parentNode.parentNode.getElementsByTagName('select')[0];	//обратиться к текущему select
-			selectedItemLocal = this.parentNode.previousSibling;							//обратиться к выбранному элементу в текущем select
+			selectedItemLocal = this.parentNode.previousSibling;							//обратиться к текущему selectedItem
 			
 			for (i = 0; i < oldSelectLocal.length; i++) {									//пройтись по всем option текущего select
-				if (oldSelectLocal.options[i].innerHTML == this.innerHTML) {				//находим option, текст в котором совпадает с текстом optionIem, по которому нажали
+				if (oldSelectLocal.options[i].innerHTML == this.innerHTML) {				//находим option, текст в котором совпадает с текстом optionItem, по которому нажали
 					oldSelectLocal.selectedIndex = i;										//устанавливаем значение select, соответствующее выбранному option
 					selectedItemLocal.innerHTML = this.innerHTML;							//текст выбранного элемента (тот, который вместо select box) установить равным optionItem, по которому нажали
 					optionItemLocal = this.parentNode.getElementsByClassName('same-as-selected');	//обратиться к элементу, который был выбран
@@ -68,3 +68,5 @@ function closeAllSelect (elmnt) {
 		}
 	}
 }
+
+document.addEventListener('click', closeAllSelect);
