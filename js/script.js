4e19717cb537ab387e9ecf157236388f3d4f3db0
kot-pirat <kot-pirat@bk.ru>
2018-07-31 14:06:42 +0300

    Мобильная версия добавил прокрутку картинок 31.07

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+var topLinks = document.querySelectorAll('.top-links-list .item-wrapper');
 var showActor = document.querySelectorAll('.actors-list .item-wrapper');
 var showCostume = document.querySelectorAll('.costumes-list .item-wrapper');
 var showVideo = document.querySelectorAll('.videos-list .video-wrapper, .videos-list .video-title');
@@ -12,29 +13,28 @@ var imageAddItem = document.querySelectorAll('.image-add-item');
 var imageNumber = 1;
 
 var orderImageLink = document.querySelectorAll('.top-order-list-description a');
-console.log(orderImageLink);
+console.log(topLinks);
 
 showCallBack.addEventListener('click', function(evt){
 	evt.preventDefault();
 	popupActor.classList.add('show-popup');
 	popupLayer.classList.add('show-popup');
 });
-for (var i = 0; i < showActor.length; i++) {
+/*for (var i = 0; i < showActor.length; i++) {
 	showActor[i].addEventListener('click', function(evt){
 		showActorPopup(evt);
 	});
+}*/
+for (var i = 0; i < showActor.length; i++) {
+	showActor[i].addEventListener('click', showActorPopup);
+}
+for (var i = 0; i < showCostume.length; i++) {
+	showCostume[i].addEventListener('click', showActorPopup);
 }
 function showActorPopup(evt) {
 	evt.preventDefault();
 	popupActor.classList.add('show-popup');
 	popupLayer.classList.add('show-popup');
-}
-for (var i = 0; i < showCostume.length; i++) {
-	showCostume[i].addEventListener('click', function(evt){
-		evt.preventDefault();
-		popupActor.classList.add('show-popup');
-		popupLayer.classList.add('show-popup');
-	});
 }
 for (var i = 0; i < showVideo.length; i++) {
 	showVideo[i].addEventListener('click', function(evt){
@@ -58,13 +58,11 @@ for (var i = 0; i < orderImageLink.length; i++) {
 		evt.preventDefault();
 		this.parentNode.parentNode.querySelector('.top-order-image').classList.add('hide-element');
 		this.parentNode.parentNode.querySelector('.top-order-image-hover').classList.remove('hide-element');
-		console.log(this.parentNode.parentNode.querySelector('.top-order-image'));
 	});
 	orderImageLink[i].addEventListener('mouseleave', function(evt){
 		evt.preventDefault();
 		this.parentNode.parentNode.querySelector('.top-order-image').classList.remove('hide-element');
 		this.parentNode.parentNode.querySelector('.top-order-image-hover').classList.add('hide-element');
-		console.log(this.parentNode.parentNode.querySelector('.top-order-image'));
 	});
 }
 
@@ -111,4 +109,60 @@ function getNewImage(imageSwitch) {
 						hoverInfo
 				'</div>'+
 		   '</a>';
+}
+
+function linkReset(evt) {
+	evt.preventDefault();
+}
+
+//-----------------------------------------------------mobile.js--------------------------------------------------------//
+
+if (screen.width < 750) {
+	var mobileClickImage = document.querySelectorAll('.js-image');
+	for (var i = 0; i < showActor.length; i++) {
+		showActor[i].removeEventListener('click', showActorPopup);
+		showActor[i].addEventListener('click', linkReset);
+	}
+	for (var i = 0; i < showCostume.length; i++) {
+		showCostume[i].removeEventListener('click', showActorPopup);
+		showCostume[i].addEventListener('click', linkReset);
+	}
+	for (var i = 0; i < topLinks.length; i++) {
+		topLinks[i].addEventListener('click', linkReset);
+	}
+
+	for (x = 0; x < mobileClickImage.length; x++) {
+		mobileClickImage[x].addEventListener('click',  function () {
+			if(this.classList.contains('js-image-left')) {
+				this.classList.remove('js-image-left');
+				this.classList.add('js-image-main');
+				this.nextElementSibling.classList.remove('js-image-main');
+				this.nextElementSibling.classList.add('js-image-right');
+				if(this.nextElementSibling.nextElementSibling) {
+					this.nextElementSibling.nextElementSibling.classList.remove('js-image-right');
+				}
+				if(this.previousElementSibling) {
+					this.previousElementSibling.classList.add('js-image-left');
+				}
+				
+				var imageListLeft = getComputedStyle(this.parentNode).left;
+				this.parentNode.style.left = parseFloat(imageListLeft.split('p')[0]) + 292 + 'px';
+			}
+			else if(this.classList.contains('js-image-right')) {
+				this.classList.remove('js-image-right');
+				this.classList.add('js-image-main');
+				this.previousElementSibling.classList.remove('js-image-main');
+				this.previousElementSibling.classList.add('js-image-left');
+				if(this.previousElementSibling.previousElementSibling) {
+					this.previousElementSibling.previousElementSibling.classList.remove('js-image-left');
+				}
+				if(this.nextElementSibling) {
+					this.nextElementSibling.classList.add('js-image-right');
+				}
+				
+				var imageListLeft = getComputedStyle(this.parentNode).left;
+				this.parentNode.style.left = parseFloat(imageListLeft.split('p')[0]) - 292 + 'px';
+			}
+		});
+	}
 }
